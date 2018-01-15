b587f866236ad1d115887baf4d0e5294d7228d2d
Edgar Robles <erobles35@gmail.com>
2018-01-15 06:53:11 +0000

    added the ability to insert data

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.List.Split
+import Database.Redis
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Char8
+
+startword = "$secretstartword$"
+endword = "$secretendword$"
 
 main :: IO ()
 main = do
+    conn <- checkedConnect defaultConnectInfo
     s <- getLine
-    print $ makeTriplet (segment s) "0"
+    print s
 
 type Wrd = String
 type Sentence = [Wrd]
 type ChatId = String
 
 segment :: String -> Sentence
-segment = splitOn " "
+segment s = startword : (splitOn " " s) ++ [endword]
 
 reverseTail = reverse . tail . reverse
 
 makeTriplet :: Sentence -> ChatId -> [(Wrd, Wrd, ChatId)]
 makeTriplet s c = zip3 (reverseTail s) (tail s) (repeat c)
+
+tripletToKey :: (Wrd, Wrd, ChatId) -> String
+tripletToKey (k1, k2, c) = k1 ++ ":" ++ k2 ++ ":" ++ c
+
+tripletToSumKey :: (Wrd, Wrd, ChatId) -> String
+tripletToSumKey (k1, _, c) = k1 ++ ":" ++ c
+
+stringToKeys :: String -> [String]
+stringToKeys s = map tripletToKey $ makeTriplet (segment s) "0"
+
+stringToSumKeys :: String -> [String]
+stringToSumKeys s = map tripletToSumKey $ makeTriplet (segment s) "0"
+
+keyToRedis :: Connection -> String -> IO ()
+keyToRedis conn key = runRedis conn $ do
+                    a <- incr (Data.ByteString.Char8.pack key)                    
+                    liftIO $ print a
+
+insertSentence :: Connection -> String -> IO ()
+insertSentence conn s = do
+    sequence_ $ map (keyToRedis conn) (stringToKeys s)
+    sequence_ $ map (keyToRedis conn) (stringToSumKeys s)
+

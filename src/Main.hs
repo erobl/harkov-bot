607e09d569d9310c72fb30437b84997f26dc5e02
Edgar <erobles35@gmail.com>
2018-01-14 23:27:02 -0600

    added the parsing functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.List.Split
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    s <- getLine
+    print $ makeTriplet (segment s) "0"
+
+type Wrd = String
+type Sentence = [Wrd]
+type ChatId = String
+
+segment :: String -> Sentence
+segment = splitOn " "
+
+reverseTail = reverse . tail . reverse
+
+makeTriplet :: Sentence -> ChatId -> [(Wrd, Wrd, ChatId)]
+makeTriplet s c = zip3 (reverseTail s) (tail s) (repeat c)
